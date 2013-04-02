a450e4c0ccdf81b93a40cd86ebd375e86dbd05e9
Mani Potnuru <mani.potnuru@gmail.com>
2013-04-02 10:40:25 -0700

    second commit after the delete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '367d64c12f48a0da7b95b169266e336f283b264f611f362715328e561fe15bc4ac41d5dfbbb5bafd3c314c9ffbf2a5a8959b79ca060f9abeff73643037aa6e66'
